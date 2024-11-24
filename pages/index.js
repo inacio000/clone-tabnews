773d37c39565150d8b88f64955d622338eacb586
Inácio Raimundo <inacioraimundo998@gmail.com>
2024-11-24 18:48:24 +0000

    feat: challenge

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+function Home() {
+    return (
+        <div>
+            <h1 align="center">No decorrer do <a href="www.curso.dev">curso.dev</a> farei parte da concorrência, mas no final eu serei um Programador F#d#</h1>
+        </div>
+    )
+}
+
+export default Home;
