484f8dff61960fa22ae726fa73f98c099fcfecce
Inácio Raimundo <inacioraimundo998@gmail.com>
2024-11-27 23:18:45 +0000

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home() {
+    return <h1 align="center">No decorrer do <a href="www.curso.dev">curso.dev</a> farei parte da concorrência, mas no final eu serei um Programador F#d#</h1>
+}
+
+export default Home;
