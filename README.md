5ca38c2ab14f4ab8a1b4655c9534075e0bbdae08
Inácio Raimundo <inacioraimundo998@gmail.com>
2024-12-01 23:05:40 +0000

    adiciona script `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Implementando a construção de um `pedaço de internet mais massa` <a href="https://www.tabnews.com.br/inacio000/e-se-eu-te-contar-como-desenvolvi-uma-solucao-recentemente-no-meu-dormitorio-usando-nodejs-e-reactjs" target="_blank">Tabnews</a>🔗 🙂
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1 align="center">O <strong>Conhecimento</strong>, ninguém tira de ti e é isso que estou recebendo no <a href="www.curso.dev">curso.dev</a> 😎</h1>
+  return (
+    <h1 align="center">
+      O <strong>Conhecimento</strong>, ninguém tira de ti e é isso que estou
+      recebendo no <a href="www.curso.dev">curso.dev</a> 😎
+    </h1>
+  );
 }
 
 export default Home;
